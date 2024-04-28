32ca5e65353fdccdb63521a20070b65638c96fee
Jorge Cujcuj <jorge.cujcuj@galileo.com>
2024-04-28 17:58:49 -0600

    Actualización de la rama principal

--- src/App.js
+++ src/App.js
@@ -8,17 +8,38 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function App() {
+  const tareas = [{
+    name: 'Dia el trabajo',
+    description: 'Celebra el dia del trabajo con tus compañeros de trabajo',
+    DueDate: '2024-05-01'
+  },
+  {
+    name: 'Dia de la madre',
+    description: 'Compra un regalo para mama y celebra con ella su dia',
+    DueDate: '2024-05-10'
+  },
+  {
+    name: 'Cumpleaños de Keyla',
+    description: 'Lleva un pastel y una tarjeta de cumpleaños a la fiesta de Keyla',
+    DueDate: '2024-05-13'
+  },
+  {
+    name: 'Tarea de la universidad',
+    description: 'Hacer la tarea de la universidad para el dia lunes',
+    DueDate: '2024-04-29'
+  }]
   return (
     <div className="App">
       <Menu></Menu>
       <Container>
       <Row>
-        <Col><Formulario></Formulario></Col>
         <Col>
-        <Item></Item>
-        <Item></Item>
-        <Item></Item>
-        <Item></Item>
+          <Formulario></Formulario>
+        </Col>
+        <Col>
+          { tareas.map((tarea, index) => {
+            return <Item name={tarea.name} description={tarea.description} DueDate={tarea.DueDate} />
+          }) }
         </Col>
       </Row>
     </Container>
--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -2,17 +2,17 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './Item.scss';
 
-function Item() {
+function Item(props) {
   return (
     /*<Card style={{ width: '18rem' }}>*/
     <Card className="mb-3">
       <Card.Body>
         <Card.Text className='fw-bold'>Name</Card.Text>
-        <Card.Text>Realizar Proyecto</Card.Text>
+        <Card.Text>{props.name}</Card.Text>
         <Card.Text className='fw-bold'>Description</Card.Text>
-        <Card.Text>Eleborar un proyecto de desarrollo de web</Card.Text>
+        <Card.Text>{props.description}</Card.Text>
         <Card.Text className='fw-bold'>Due Date</Card.Text>
-        <Card.Text>dd/mm/aaaa</Card.Text>
+        <Card.Text>{props.DueDate}</Card.Text>
         <Button variant="info">Remover</Button>
       </Card.Body>
     </Card>
